4af3857e9d868d499cfc06697cb42fb3723f3374
aleksey leichenko <ol.leichenko@gmail.com>
2019-01-29 19:21:57 +0200

    add and delete elements to html by js events

--- getElement-from-page-DOM/js.js
+++ getElement-from-page-DOM/js.js
@@ -1,7 +1,8 @@
 const e = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByTagName('button'),
-    heart = document.querySelectorAll('.heart');
+    heart = document.querySelectorAll('.heart'),
+    wrapper = document.querySelector('.wrapper');
 
 console.log(e);
 
@@ -16,3 +17,24 @@ but3.onclick = function () {
 };
 
 console.log(heart);
+heart.forEach(function (item, i, heartArray) {
+   item.style.backgroundColor = 'yellow';
+});
+
+//create element
+let div = document.createElement('div'),
+    text = document.createTextNode('Here I am');
+
+div.classList.add('black');
+
+div.innerHTML = '<h1>Hello world!</h1>' //no protect because user can send bad script
+
+div.textContent = 'Hello world, man!'; //it's protect to use
+
+// document.body.appendChild(div); //insert element to the and of body
+// wrapper.appendChild(div); //insert element to end of parent wrapper
+document.body.insertBefore(div, wrapper); //insert element into ahead parent element
+wrapper.removeChild(heart[0]);
+
+
+console.log(div);
