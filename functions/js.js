020ab2b4d8c51ee88672a794378c89f7e751c013
aleksey leichenko <ol.leichenko@gmail.com>
2019-01-29 14:14:37 +0200

    functions

---
+++ functions/js.js
@@ -0,0 +1,17 @@
+function sum(a,b) {
+    return (a+b);
+}
+console.log(sum(2,5));
+
+console.log("----------------------------")
+
+let example = function (sum) {
+    return console.log(sum)
+};
+
+example("Joy");
+console.log("----------------------------")
+
+const multiply = (a1, a2) => a1*a2;
+console.log(multiply(2,3));
+console.log("----------------------------")
