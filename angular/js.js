443f32af07460683cd34d22ece459a456980b5d0
aleksey leichenko <alekseyleychenko@gmail.com>
2019-02-12 14:48:42 +0200

    framework angular//react/vue

---
+++ angular/js.js
@@ -0,0 +1,22 @@
+
+
+//instruments for work with angular
+
+/*
+1. nodejs -> npm
+2. TypeScript
+3. Webpack
+4. MVC (model - view - controller)
+5. Angular (angular.io/docs)*/
+
+
+//instruments for work with react
+/*1. nodejs -> npm
+2. babeljs.io
+3. jsx
+4. react (reactjs.org)*/
+
+//instruments for work with Vue
+/*1. nodejs -> npm
+2. babel
+3. Webpack*/
