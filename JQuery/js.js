ad562792a016e25be7bacdaa71955816e7876bc5
aleksey leichenko <alekseyleychenko@gmail.com>
2019-02-12 13:14:23 +0200

    jquery basic //animation

---
+++ JQuery/js.js
@@ -0,0 +1,26 @@
+ $(document).ready(function () {
+     $('.list-item:first').hover(function () {
+         $(this).toggleClass('active')
+     });
+
+     //animation
+     $('.list-item:eq(2)').on('click', function () {
+         $('.image:even').fadeToggle('slow');
+     });
+
+     $('.list-item:eq(4)').on('click', function () {
+         $('.image:odd').animate(
+             {
+              opacity: 'toggle',
+              height: 'toggle'
+             }, 2000
+         );
+     });
+ });
+
+    //в нативном js можно уже и так делать
+ //  document.querySelectorAll('.list-item').forEach();
+ // .classList
+ // .addEventListener
+ // .$ajax  - fetch(в нативном)
+ // animations
